9a2d7db0657ff9c726a5c0360f55602c0408481f
Simon Marchi <simon.marchi@ericsson.com>
2015-07-03 11:35:31 -0400

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GDB check script',
-    version='1.0.1',
+    version='1.0.2',
     packages=['gdbcheck'],
     entry_points={
         'console_scripts': [
