f77801de349e565060c191b5c51489d79d1a7ae8
Simon Marchi <simon.marchi@polymtl.ca>
2020-11-15 15:23:24 -0500

    Run black on setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,15 @@ from setuptools import setup
 import sys
 
 if sys.version_info.major != 3:
-    print('gdb-check needs Python 3.')
+    print("gdb-check needs Python 3.")
     sys.exit(1)
 
 setup(
-    name='GDB check script',
-    version='1.0.3',
-    packages=['gdbcheck'],
-    entry_points={
-        'console_scripts': [
-            'gdb-check = gdbcheck.gdbcheck:main'
-        ]
-    },
+    name="GDB check script",
+    version="1.0.3",
+    packages=["gdbcheck"],
+    entry_points={"console_scripts": ["gdb-check = gdbcheck.gdbcheck:main"]},
     install_requires=[
-        'termcolor',
-    ]
+        "termcolor",
+    ],
 )
